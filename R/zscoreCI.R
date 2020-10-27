7569ee904030e6c1a6665922ec4328101ceaa190
lin-jennifer <jennifer.lin16@ncf.edu>
2020-10-27 12:15:03 -0400

    Confidence Intervals based on z distribution

---
+++ R/zscoreCI.R
@@ -0,0 +1,54 @@
+#' Confidence Interval from a Normal Distribution
+#'
+#' Helpful functions to calculate a confidence interval
+#' using a z-distribution
+#'
+#' NOTE: This is not NOT intended from a t-distribution.
+#' See relavant tscore file for those calculations
+#'
+#' @name zscoreCI
+#'
+#' @param x x-bar for observed mean
+#' @param se Standard Error -- see se()
+#' @param ci Confidence Interval Level
+#' @param sd Standard Deviation -- reference sdNA()
+#' @param n Number of participants
+#'
+#' @seealso sdNA()
+#'
+#' @examples
+#' x = 3.5
+#' sd = 2
+#' n = 25
+#'
+#' se <- se(sd, n)  # 0.4
+#'
+#' ci = 0.95  # for 95% Confidence Interval
+#'
+#' zCIupper(x, se, ci) # 4.283986
+#' zCIlower(x, se, ci) # 2.716014
+#' zCI(se, ci)         # 0.7839856
+#'
+#' @export
+se <- function(sd, n){
+  sd/sqrt(n)
+}
+
+#' @name zscoreCI
+#' @export
+zCIupper <- function(x, se, ci){
+  x + (qnorm((ci + ((1-ci)/2))))*se
+}
+
+#' @name zscoreCI
+#' @export
+zCIlower <- function(x, se, ci){
+  x - (qnorm((ci + ((1-ci)/2))))*se
+}
+
+#' @name zscoreCI
+#'
+#' @export
+zCI <- function(se, ci){
+  (qnorm((ci + ((1-ci)/2))))*se
+}
